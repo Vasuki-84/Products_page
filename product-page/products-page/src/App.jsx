76a6a569aa3fa149d8e52f6e8e459c0b046d08b2
Vasuki t <iamvasukithangaraj@gmail.com>
2025-10-14 11:30:29 +0530

    added Router

--- product-page/products-page/src/App.jsx
+++ product-page/products-page/src/App.jsx
@@ -1,18 +1,20 @@
-import React from 'react'
-import Navbar from './components/navbar'
-import Home from './components/Home'
-import Footer from './components/Footer'
-
+import React from "react";
+import Navbar from "./components/navbar";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div>
       <Navbar />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+
       <Footer />
-    
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
