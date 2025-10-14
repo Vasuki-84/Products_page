8f35e248da8818bace8b1df9df41fe0263cc4fa5
Vasuki t <iamvasukithangaraj@gmail.com>
2025-10-14 15:02:19 +0530

    added products details section

--- product-page/products-page/src/components/Footer.jsx
+++ product-page/products-page/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='bg-yellow-300 p-2 text-center text-sm'>
+    <div className=' fixed bottom-0 left-0 right-0 w-full bg-yellow-300 p-2 text-center text-sm'>
         <p>&copy;{new Date().getFullYear()} | Products_Page. All Rights Reserved 
         </p>
     </div>
--- product-page/products-page/src/components/ProductDetails.jsx
+++ product-page/products-page/src/components/ProductDetails.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, {useState , useEffect } from "react";
 
 function ProductDetails() {
+  const [product, setProduct] = useState({});
+
+  useEffect(() => {
+    fetch(`https://fakestoreapi.com/products/3`)
+      .then((res) => res.json())
+      .then(data => setProduct(data))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
-    <div className='p-8'>
-        <h2 className='text-2xl'>ProductDetails</h2></div>
-  )
+    <div className="mt-20 h-screen">
+      <h2 className="text-2xl">Product Detail Pages</h2>
+      <div>
+        <div>
+          <img src={product?.image} alt="ProductsImage" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default ProductDetails
+export default ProductDetails;
