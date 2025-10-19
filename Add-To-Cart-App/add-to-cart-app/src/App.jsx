6fec62b405218a79bd1f9a1e10ec9ba4e22bdb2d
Vasuki t <iamvasukithangaraj@gmail.com>
2025-10-19 20:06:11 +0530

    added nav bar

--- Add-To-Cart-App/add-to-cart-app/src/App.jsx
+++ Add-To-Cart-App/add-to-cart-app/src/App.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import Home from "./Components/Home";
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import Cart from "./Components/Cart";
 
 function App() {
   return (
-    <div>App</div>
-  )
+     <div>
+      <nav className='flex justify-between items-center px-8 py-4 bg-white shadow-md'>
+        <h1 className='text-2xl font-bold text-gray-800'>Cartify</h1>
+        <div className='space-x-4'>
+          <Link className='text-blue-600 font-semibold' to="/">Home</Link>
+          <Link className='text-blue-600 font-semibold' to="/cart">My Cart</Link>
+        </div>
+      </nav>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/cart' element={<Cart />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
