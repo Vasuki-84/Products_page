3ec1766847fe075b3cf8ac1085455f308a46ba64
Vasuki t <iamvasukithangaraj@gmail.com>
2025-10-21 13:39:40 +0530

    imported provider in main.jsx

--- Add-To-Cart-App/add-to-cart-app/src/main.jsx
+++ Add-To-Cart-App/add-to-cart-app/src/main.jsx
@@ -5,10 +5,13 @@ import App from './App.jsx'
 import {BrowserRouter} from "react-router-dom"
 import "react-toastify/dist/ReactToastify.css"
 import { Slide, ToastContainer } from 'react-toastify'
+import {Provider} from "react-redux"
+import { store } from './redux/store.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
+    <Provider store={store}>
         <App />
         <ToastContainer
          position='bottom-right'
@@ -17,7 +20,7 @@ createRoot(document.getElementById('root')).render(
          theme='dark'
          transition={Slide}
         />
-
+</Provider>
     
 
     </BrowserRouter>
